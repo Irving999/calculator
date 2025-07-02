c499f686076194c0f4368458077800fe040afdd6
Irving Bautista <irvingbautista681@gmail.com>
2025-07-02 13:54:53 -0700

    feat: add operate function that takes operator and two numbers to perform basic operation

--- script.js
+++ script.js
@@ -16,3 +16,18 @@ function divide(...args) {
     if (args.length === 0) return 0;
     return args.reduce((total, current) => total / current);
 }
+
+function operate(operator, a, b) {
+    switch(operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default:
+            return 'Operation is not supported';
+    }
+}
